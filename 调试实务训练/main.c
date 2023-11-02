e2cfe43069ae83715f3e663e6a2c76a65984a066
MissMary-ts2020 <hkgjwtl2017@aliyun.com>
2023-11-02 14:47:25 +0800

    添加项目文件。

---
+++ 调试实务训练/main.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main() {
+
+	int i = 0;
+	int sum = 0;//保存最终结果
+	int n = 0;
+	int ret = 1;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		int j = 0;
+		for (j = 1; j <= i; j++) {
+			ret *= j;
+		}
+		sum += ret;
+	}
+	printf("%d\n", sum);
+
+
+
+	return NULL;
+}
