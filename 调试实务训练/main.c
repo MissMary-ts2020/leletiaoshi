56ecc4f8056272bcb7565cf8ca2f666a632742a5
MissMary-ts2020 <hkgjwtl2017@aliyun.com>
2023-11-02 16:10:50 +0800

    16点10分

--- 调试实务训练/main.c
+++ 调试实务训练/main.c
@@ -3,15 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
-void my_strcpy(char* dest, char* source) {
-	while (*source != '\0') {
-		*dest = *source;
-		dest++, source++;
+void my_strcpy(char* dest,const char* source) {//const无法被改变
+	assert(dest != NULL);
+	assert(source != NULL);//断言,危险时可以断开
+	while (*dest++ = *source++);//要学会这种神经病写法
+		//当正好为'\0'也就是ASCII0的时候，正好就。。。。。
+		// 
+		//此时指针已经指向了source的\0，而dest的位置指向了dest应当为\0的位置
 
-	}
-	//此时指针已经指向了source的\0，而dest的位置指向了dest应当为\0的位置
-	*dest = '\0';//因此这里要补一个
+	return;
+	
 
 }
 
@@ -19,10 +22,11 @@ int main() {
 
 	//strcpy
 	//字符串拷贝
-	char str1[] = "accesssss";
-	char str2[] = "cc";
-	my_strcpy(str1, str2);
-	printf("%s\n%s\n", str1, str2);
+	const int num = 19;//num不允许更改
+	const int* p = &num;//const放在指针变量的*左边，修饰的是*p,设置该指针为只读指针
+	int** pp = &p;
+	**pp = 20;//可以通过更高级别的指针强制更改
+	printf("%d", *p);
 
 
 	return NULL;
