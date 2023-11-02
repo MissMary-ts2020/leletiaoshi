3ba9f29b9e46ba7b5fb0f8d12e39c759c63f2a77
MissMary-ts2020 <hkgjwtl2017@aliyun.com>
2023-11-02 15:05:11 +0800

    2023年11月2日15点05分

--- 调试实务训练/main.c
+++ 调试实务训练/main.c
@@ -1,22 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
 	int i = 0;
-	int sum = 0;//保存最终结果
-	int n = 0;
-	int ret = 1;
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++) {
-		int j = 0;
-		for (j = 1; j <= i; j++) {
-			ret *= j;
-		}
-		sum += ret;
+	int arr[10] = { 0 };
+	for (; i < 10; i++) {
+		arr[i] = i + 1;
 	}
-	printf("%d\n", sum);
+
+	for (i = 0; i <= 12; i++) {
+		printf("always");
+		arr[i] = 0;
+	}
+
+	system("pause");
+	//研究该程序死循环的原因
 
 
 
